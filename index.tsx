23d4137899c1e141766b17e2353cd256ddacf5b3
Nick Nance <nance.nick@gmail.com>
2020-04-19 08:37:16 -0700

    draw tetronmino

--- index.tsx
+++ index.tsx
@@ -9,25 +9,175 @@ enum Actions {
   gameLoop,
 }
 
+type Color = string;
+
+type Matrix = Color[][];
+
+type Tetromino = {
+  matrix: Matrix;
+  col: number;
+  row: number;
+};
+
 type GameState = {
   score: number;
   level: number;
+  board: Matrix;
+  piece: Tetromino;
+  nextPiece: Tetromino;
 };
 
 type GameStore = [GameState, React.Dispatch<Actions>];
+
+const GRID = 32;
+const WALLSIZE = 1;
 
 const CANVAS = {
   width: 320,
   height: 640,
 };
 
+type TETROMINOCODES = "T" | "I" | "J" | "L" | "O" | "S" | "Z";
+
+// how to draw each tetromino
+// @see https://tetris.fandom.com/wiki/SRS
+const TETROMINOS = {
+  I: [
+    [0, 0, 0, 0],
+    [1, 1, 1, 1],
+    [0, 0, 0, 0],
+    [0, 0, 0, 0],
+  ],
+  J: [
+    [1, 0, 0],
+    [1, 1, 1],
+    [0, 0, 0],
+  ],
+  L: [
+    [0, 0, 1],
+    [1, 1, 1],
+    [0, 0, 0],
+  ],
+  O: [
+    [1, 1],
+    [1, 1],
+  ],
+  S: [
+    [0, 1, 1],
+    [1, 1, 0],
+    [0, 0, 0],
+  ],
+  Z: [
+    [1, 1, 0],
+    [0, 1, 1],
+    [0, 0, 0],
+  ],
+  T: [
+    [0, 1, 0],
+    [1, 1, 1],
+    [0, 0, 0],
+  ],
+};
+
+// color of each tetromino
+const COLORS = {
+  I: "cyan",
+  O: "yellow",
+  T: "purple",
+  S: "green",
+  Z: "red",
+  J: "blue",
+  L: "orange",
+};
+
+const tetrominoToMatrix = (tetromino: number[][], color: Color): Matrix => {
+  return tetromino.map((row) => row.map((col) => (col ? color : "")));
+};
+
+function getProperty<T, K extends keyof T>(o: T, propertyName: K): T[K] {
+  return o[propertyName]; // o[propertyName] is of type T[K]
+}
+
+const createPiece = (): Matrix => {
+  const keys = Object.keys(TETROMINOS);
+  const pieceIndex = Math.floor(Math.random() * keys.length);
+  const key = keys[pieceIndex] as TETROMINOCODES;
+  return tetrominoToMatrix(
+    getProperty(TETROMINOS, key),
+    getProperty(COLORS, key)
+  );
+};
+
+const createRow = () => Array(10).fill("");
+
 const createState = (): GameState => ({
   score: 0,
   level: 0,
+  piece: { matrix: createPiece(), col: 3, row: -1 },
+  nextPiece: { matrix: createPiece(), col: 1, row: 0 },
+  board: Array(20).fill(createRow())
 });
 
-const drawBoard = (ctx: CanvasRenderingContext2D) => {
+/**
+ *
+ * Drawing Functions
+ *
+ */
+
+const drawTetronminoBlock = (
+  ctx: CanvasRenderingContext2D,
+  tetromino: Tetromino,
+  col: number,
+  row: number
+): void => {
+  ctx.fillStyle = tetromino.matrix[row][col];
+
+  // drawing 1 px smaller than the grid creates a grid effect
+  const x = (tetromino.col + col) * GRID;
+  const y = (tetromino.row + row) * GRID;
+  const w = GRID - 1;
+  const h = GRID - 1;
+  ctx.fillRect(x, y, w, h);
+};
+
+const drawTetronmino = (
+  ctx: CanvasRenderingContext2D,
+  tetromino: Tetromino
+): void => {
+  tetromino.matrix.forEach((cols, row) => {
+    cols.forEach((on, col) => {
+      if (on) drawTetronminoBlock(ctx, tetromino, col, row);
+    });
+  });
+};
+
+const fillBoard = (ctx: CanvasRenderingContext2D, board: Matrix): void => {
+  board.forEach((cols, row) => {
+    cols.forEach((on, col) => {
+      if (on) {
+        ctx.fillStyle = on;
+        // drawing 1 px smaller than the grid creates a grid effect
+        ctx.fillRect(col * GRID, row * GRID, GRID - 1, GRID - 1);
+      }
+    });
+  });
+};
+
+const drawWalls = (ctx: CanvasRenderingContext2D) => {
+      // draw walls
+  ctx.fillStyle = "lightgrey";
+  ctx.fillRect(0, 0, CANVAS.width, WALLSIZE);
+  ctx.fillRect(0, 0, WALLSIZE, CANVAS.height);
+  ctx.fillRect(CANVAS.width - WALLSIZE, 0, WALLSIZE, CANVAS.height);
+  ctx.fillRect(0, CANVAS.height - WALLSIZE, CANVAS.width, WALLSIZE);
+}
+
+const drawBoard = (ctx: CanvasRenderingContext2D, state: GameState) => {
   ctx.clearRect(0, 0, CANVAS.width, CANVAS.height);
+
+  fillBoard(ctx, state.board);
+  drawTetronmino(ctx, state.piece);
+  drawWalls(ctx);
 };
 
 const reducer = (state: GameState, action: Actions) => {
@@ -36,19 +186,14 @@ const reducer = (state: GameState, action: Actions) => {
 
 const GameBoard = () => {
   const canvasRef = React.useRef<HTMLCanvasElement>(null);
+  const [state] = React.useContext(GameContext);
 
   React.useEffect(() => {
     const ctx = canvasRef.current?.getContext("2d");
-    if (ctx) drawBoard(ctx);
-  }, [canvasRef]);
+    if (ctx) drawBoard(ctx, state);
+  }, [canvasRef, state]);
 
-  return (
-    <canvas
-      ref={canvasRef}
-      width={CANVAS.width}
-      height={CANVAS.height}
-    />
-  );
+  return <canvas ref={canvasRef} width={CANVAS.width} height={CANVAS.height} />;
 };
 
 const Theme: React.FC = (props) => {
